dff9b16c0e1085e86e761b277b8048c71d58a4e5
Cody Logan <cody@madrants.net>
2017-08-01 13:01:42 -0700

    Add some basic initial tests

--- spec/cpiu_spec.rb
+++ spec/cpiu_spec.rb
@@ -4,8 +4,22 @@ RSpec.describe CPIU do
   it 'has a version number' do
     expect(CPIU::VERSION).not_to be nil
   end
+end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+RSpec.describe CPIU::API do
+  it 'has the correct URL' do
+    expect(CPIU::API::URL).to eql('https://api.bls.gov/publicAPI/v2/timeseries/data/')
+  end
+
+  it 'has the correct series ID' do
+    expect(CPIU::API::SERIESID).to eql('CUUR0000SA0')
+  end
+
+  context 'when getting data for a single year' do
+    it 'should return an array with a single element'
+  end
+
+  context 'when getting data for a range of years' do
+    it 'should return an array with multiple elements'
   end
 end
